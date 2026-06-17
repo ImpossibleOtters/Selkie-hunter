ae07bffb0a9de610f9b1d34d21d933afeb2a32a9
ImpossibleOtters <fleurmwillis@gmail.com>
2026-06-17 19:46:12 +0100

    Implement search functionality for Selkie terms
    Added search functionality for specific terms related to Selkie.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Selkie Hunter is alive!")
+import requests
+
+SEARCH_TERMS = [
+    "Selkie Scottish Plaid Astronomer Gown",
+    "Scotland Plaid Astronomer",
+    "Scottish Plaid Astronomers Gown",
+    "Selkie Plaid Astronomer",
+]
+
+print("🔎 Selkie Hunter running")
+
+for term in SEARCH_TERMS:
+    print(f"Searching for: {term}")
+
+print("Done")
